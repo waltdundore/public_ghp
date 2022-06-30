b962ab8a7fcb49878fad01ea9e264c84c14f5d23
Walter Dundore <wdundore@westga.edu>
2022-06-30 15:42:45 -0400

    new

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#....
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+#
+
+apt-get update -y && apt-get dist-upgrade -y && apt autoremove
+
+apt install git wireshark -y
+
+#install and customize docker
+apt-get -y install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update -y
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+docker run hello-world
